110330dcec4d0190a42f1045e6d298bde10be14d
kashireddy <kashireddynaani9967@gmail.com>
2023-10-25 17:30:38 +0530

    infra prod

--- vars/nodeJSVMCI.groovy
+++ vars/nodeJSVMCI.groovy
@@ -81,7 +81,7 @@ def call(Map configMap){
                     script{
                         echo "Deployment"
                         def params = [
-                            string(name: 'version', value: "$packageVersion"),
+                            string(name: 'version', value: "$packageVersion")
                             // string(name: 'environment', value: "dev")
                         ]
                         build job: "../${component}-deploy", wait: true, parameters: params
