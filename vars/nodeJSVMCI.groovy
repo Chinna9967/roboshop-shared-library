cff0bfb68bcef08c45cee8e271697f785115d8ac
kashireddy <kashireddynaani9967@gmail.com>
2023-10-25 17:35:46 +0530

    infra prod

--- vars/nodeJSVMCI.groovy
+++ vars/nodeJSVMCI.groovy
@@ -1,107 +1,92 @@
 def call(Map configMap){
-    // mapNmae.get("key-name")
-    echo "component is : $component"
+    // mapName.get("key-name")
     def component = configMap.get("component")
+    echo "component is : $component"
     pipeline {
-        agent { node { label 'AGENT-1' } } 
-        options{
-            ansiColor('xterm')
-        }
+        agent { node { label 'AGENT-1' } }
         environment{
+            //here if you create any variable you will have global access, since it is environment no need of def
             packageVersion = ''
         }
-        // parameters {
-        //     string(name: 'component', defaultValue: ' ', description: 'Which component?')
-        // }
+        
         stages {
-            // push to featire branch
-            stage('Get Version'){
+            stage('Get version'){
                 steps{
                     script{
                         def packageJson = readJSON(file: 'package.json')
                         packageVersion = packageJson.version
-                        echo "${packageVersion}"
+                        echo "version: ${packageVersion}"
                     }
                 }
             }
-            stage('Install Dependencies') {
+            stage('Install depdencies') {
                 steps {
                     sh 'npm install'
                 }
             }
-            stage('Unit test'){
-                steps{
-                    echo "unit test is done here"
+            stage('Unit test') {
+                steps {
+                    echo "unit testing is done here"
                 }
             }
-            stage('sonar-scan'){
-                steps{
-                    // sh 'ls -ltr'
-                    // sh 'sonar-scanner'
+            //sonar-scanner command expect sonar-project.properties should be available
+            stage('Sonar Scan') {
+                steps {
                     echo "Sonar scan done"
                 }
             }
-            stage('Build'){
-                steps{
+            stage('Build') {
+                steps {
                     sh 'ls -ltr'
-                    sh 'zip -r ${component}.zip ./* --exclude=.git --exclude=.zip'
+                    sh "zip -r ${component}.zip ./* --exclude=.git --exclude=.zip"
                 }
             }
-            stage('SAST'){
-                steps{
-                    echo "SAST DOne"
+            stage('SAST') {
+                steps {
+                    echo "SAST Done"
                     echo "package version: $packageVersion"
                 }
             }
-            // install pipeline utility steps plugin
-            stage('Pushing arificato to nexus'){
+            //install pipeline utility steps plugin, if not installed
+            stage('Publish Artifact') {
                 steps {
                     nexusArtifactUploader(
-                    nexusVersion: 'nexus3',
-                    protocol: 'http',
-                    nexusUrl: '184.73.110.135:8081/',
-                    groupId: 'com.roboshop',
-                    version: "$packageVersion",
-                    repository: "${component}",
-                    credentialsId: 'nexus-auth',
-                    artifacts: [
-                        [artifactId: "${component}",
-                        classifier: '',
-                        file: "${component}.zip",
-                        type: 'zip']
-                    ]
-                )
-                            }
+                        nexusVersion: 'nexus3',
+                        protocol: 'http',
+                        nexusUrl: '184.73.110.135:8081/',
+                        groupId: 'com.roboshop',
+                        version: "$packageVersion",
+                        repository: "${component}",
+                        credentialsId: 'nexus-auth',
+                        artifacts: [
+                            [artifactId: "${component}",
+                            classifier: '',
+                            file: "${component}.zip",
+                            type: 'zip']
+                        ]
+                    )
+                }
             }
-            // here i need to configure downstream job. i have to pass pkg version for deployment
-            // this job will wait until downstream job is over
-            // By default when a non-master branch CI is done, we can go for DEV developement
-            stage('Deploy'){
-                steps{
+
+            //here I need to configure downstram job. I have to pass package version for deployment
+            // This job will wait until downstrem job is over
+            stage('Deploy') {
+                steps {
                     script{
                         echo "Deployment"
                         def params = [
                             string(name: 'version', value: "$packageVersion")
-                            // string(name: 'environment', value: "dev")
                         ]
                         build job: "../${component}-deploy", wait: true, parameters: params
                     }
-                    
                 }
             }
-
         }
-        // post{
-        //     always{
-        //         // need to delete workspace everytime
-        //         echo "cleaning up workspace"
-        //         //deleteDir()
-        //     }
-        // }
+
         post{
             always{
                 echo 'cleaning up workspace'
-                deleteDir()
+                //deleteDir()
             }
         }
     }
--- vars/pipelineDecission.groovy
+++ vars/pipelineDecission.groovy
@@ -1,19 +1,18 @@
 #!groovy
 
-def decidePipeline(Map configMap){
+def decidePipleine(Map configMap){
     application = configMap.get("application")
-    // here we aregetiing nodejs vm
+    //# here we are getting nodeJSVM
     switch(application) {
         case 'nodeJSVM':
+            echo "application is Node JS and VM based"
             nodeJSVMCI(configMap)
-            echo "application is NOdejs and VM based"
-        break
-        case 'javaVM':
+            break
+        case 'JavaVM':
             javaVMCI(configMap)
-        break
+            break
         default:
             error "Un recognised application"
-        break
+            break
     }
-    // echo "i need to take decision based on map you sent"
 }
